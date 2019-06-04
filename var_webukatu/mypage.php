e546c3188dac1bb64247c61d56feb5791a67dbdd
Ironbrain <mfuxic@gmail.com>
2019-06-04 12:55:11 +0900

    一時保存

---
+++ var_webukatu/mypage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+  <head>
+    <meta charset="UTF-8">
+    <title>マイページ</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+
+  <body>
+    <p>あなたのメールアドレスは <?php  ?> です。 </p>
+    <p>あなたのパスワードは <?php  ?> です。</p>
+    <a href="index.html">ユーザー登録へ戻る</a>
+
+  </body>
+</html>
